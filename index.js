0859eb754b6a073bac850f4efae02c099a401bbf
Ebenezer-15 <ebnezerosaerhabor@gmail.com>
2022-06-28 01:04:31 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+    // document.querySelector(".web").addEventListener("click", web);
+    // function web(){
+    //     .web.st
+
+    // }
+    //  const web = document.getElementById('web');
+
+// web.addEventListener('click', function onClick(event) {
+  // 👇️ change background color
+//   document.body.style.backgroundColor = 'red';
+
+  // 👇️ optionally change text color
+  // document.body.style.color = 'white';
+// });
+
